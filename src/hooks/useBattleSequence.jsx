2249ead3e971593ea36d7da56ecbe147b9e9dad9
Luke G <lukejgranered@gmail.com>
2022-07-19 12:12:18 -0500

    magic case added

--- src/hooks/useBattleSequence.jsx
+++ src/hooks/useBattleSequence.jsx
@@ -27,7 +27,7 @@ export const useBattleSequence = sequence => {
             const receiver = turn === 0 ? opponentStats : playerStats;
 
             switch (mode) {
-                case 'attack':
+                case 'attack': {
                     const damage = attack({ attacker, receiver });
 
                     (async () => {
@@ -67,9 +67,52 @@ export const useBattleSequence = sequence => {
                         setInSequence = false;
                     })();
                     break;
+                }
 
-                default:
+                case 'magic': {
+                    const damage = magic({ attacker, receiver });
+
+                    (async () => {
+                        setInSequence(true);
+                        setAnnoncerMessage(`${attacker.name} has cast a spell!`);
+
+                        await wait(1000);
+
+                        turn === 0 
+                        ? setPlayerAnimation('magic') 
+                        : setOpponentAnimation('magic');
+                        await wait(100);
+
+                        turn === 0 
+                        ? setPlayerAnimation('static') 
+                        : setOpponentAnimation('static');
+                        await wait(500);
+
+                        turn === 0 
+                        ? setOpponentAnimation('damage') 
+                        : setPlayerAnimation('damage');
+                        await wait(750);
+
+                        turn === 0 
+                        ? setOpponentAnimation('static') 
+                        : setPlayerAnimation('static');
+                        setAnnoncerMessage(`${receiver.name} doesn't know what hit them!`);
+                        turn === 0 
+                        ? setOpponentHealth(h => (h - damage > 0 ? h - damage : 0)) 
+                        : setplayerHealth(h => (h - damage > 0 ? h - damage : 0));
+                        await wait(2000);
+
+                        setAnnoncerMessage(`Now its ${receiver.name} turn!`);
+                        await wait(1500);
+
+                        setTurn(turn === 0 ? 1 : 0);
+                        setInSequence = false;
+                    })();
                 break;
+            }
+                default: {
+                break;
+                }
             }
         }
     }, [sequence]);
