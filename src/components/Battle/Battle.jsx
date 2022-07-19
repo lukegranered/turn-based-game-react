c05ecc43618a5ecbae26f7fcf8f783c0198b3cf3
Luke G <lukejgranered@gmail.com>
2022-07-19 10:46:52 -0500

    player and opponent props added

--- src/components/Battle/Battle.jsx
+++ src/components/Battle/Battle.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { opponentStats, playerStats } from '../../shared';
 import { PlayerSummary } from '../PlayerSummary'
 import styles from './styles.module.css'
 
 export const Battle = () => {
+  const [playerHealth, setplayerHealth] = useState(playerStats.maxHealth);
+  const [opponentHealth, setOpponentHealth] = useState(opponentStats.maxHealth);
   return (
     <div className={styles.main}>
         <div className={styles.opponent}>
             <div className={styles.summary}>
-              <PlayerSummary main={false}/>
+              <PlayerSummary 
+                health={opponentHealth}
+                name={opponentStats.name}
+                level={opponentStats.level}
+                maxHealth={opponentStats.maxHealth}
+              />
             </div>
         </div>
         <div className={styles.user}>
           <div className={styles.summary}>
-            <PlayerSummary main/>
+            <PlayerSummary 
+            main
+            health={playerHealth}
+            name={playerStats.name}
+            level={playerStats.level}
+            maxHealth={playerStats.maxHealth}
+            />
           </div>
         </div>
     </div>
--- src/components/PlayerSummary/PlayerSummary.jsx
+++ src/components/PlayerSummary/PlayerSummary.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { Bar } from '../Bar';
 
 const red = '#821200';
 const blue = '#1953CB';
 
-export const PlayerSummary = ({ main = false }) => {
+export const PlayerSummary = ({ main = false, name, level, health, maxHealth }) => {
   return (
     <div 
     style={{ backgroundColor: main ? red : blue }} 
-    className={styles.main}>PlayerSummary</div>
+    className={styles.main}>
+        <div className={styles.info}>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.level}>lvl: {level}</div>
+        </div>
+
+        <div className={styles.health}>
+            <Bar label='HP' value={health} maxValue={maxHealth} />
+        </div>
+    </div>
   )
 }
